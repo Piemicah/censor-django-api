a116cca1056c2c4f06269fe5b1090e350fbba0c7
Piemicah <nuges1.62@gmail.com>
2024-08-03 00:22:25 +0100

    refresh added

--- censor_api/urls.py
+++ censor_api/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("persons/", views.PersonListCreate.as_view(), name="person_list"),
     path("persons/<int:pk>/", views.PersonRUD.as_view(), name="person_detail"),
+    path("manage_tokens/", views.SaveApi.as_view()),
+    path("logout/", views.Logout.as_view()),
     # path("persons/edit/<int:pk>/", views.UpdatePersonView.as_view(), name="update"),
     # path("persons/delete/<int:pk>/", views.DeletePersonView.as_view(), name="delete"),
 ]
--- censor_api/views.py
+++ censor_api/views.py
@@ -5,6 +5,10 @@ from rest_framework import generics
 from rest_framework import permissions
 from .serializers import PersonSerializer, UserSerializer
 from censor.models import Persons
+from rest_framework.parsers import MultiPartParser, JSONParser
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework.views import APIView
 
 
 class PersonListCreate(generics.ListCreateAPIView):
@@ -32,3 +36,32 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
+
+
+tokens = {"token": "", "refresh": "", "user": ""}
+
+
+class SaveApi(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        return Response(tokens)
+
+    def post(self, request):
+        token = request.data["token"]
+        refresh = request.data["refresh"]
+        user = request.data["user"]
+        tokens["token"] = token
+        tokens["refresh"] = refresh
+        tokens["user"] = user
+        return Response({"status": "success"})
+
+
+class Logout(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        tokens["token"] = ""
+        tokens["refresh"] = ""
+        tokens["user"] = ""
+        return Response({"status": "success"})
